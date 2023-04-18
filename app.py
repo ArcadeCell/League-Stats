3ead15dec89890667ce8be8392d6e23a2d70b06e
Kevin <92705109+ArcadeCell@users.noreply.github.com>
2023-04-17 23:39:30 -0700

    New Website

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def index():
         table_html = table.get_html_string()
         
         return render_template("index.html", table=table_html)
-
-    return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
