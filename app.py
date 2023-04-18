55eff117da75de9c4bcf8bf40660234ebd55fb15
Kevin <92705109+ArcadeCell@users.noreply.github.com>
2023-04-17 21:26:56 -0700

    Second commit

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ from flask import Flask, render_template, request
 from data import champion_dict, api_key, get_champion_mastery_data, organize_champion_data
 from prettytable import PrettyTable
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-def get_stats(summoner_name):
-    data = get_champion_mastery_data(api_key, summoner_name)
-    if data == -1:
-        return {'error': 'Summoner does not exist. Please try again.'}
-    else:
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        summoner_name = request.form["summoner_name"]
+        data = get_champion_mastery_data(api_key, summoner_name)
+        while data == -1: 
+            summoner_name = str(input("Summoner does not exist. Please try again: "))
+            data = get_champion_mastery_data(api_key, summoner_name)
+        
         data = organize_champion_data(data, champion_dict)
+
+        # print table
         table = PrettyTable()
         table.junction_char = "o"
         table.add_column("Champion", data["champion_names"])
@@ -18,21 +24,21 @@ def get_stats(summoner_name):
         table.add_column("Last Played(Days)", data["last_played"])
         table.add_column("Mastery Points Until Next Level", data["points_until_next_level"])
         table.add_column("Chest Granted", data["chests_granted"])
-        table.sortby = "Chest Granted"
-        return {'table': table.get_html_string()}
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        summoner_name = request.form['summoner_name']
-        stats = get_stats(summoner_name)
-        if 'error' in stats:
-            return render_template('index.html', error=stats['error'])
-        else:
-            return render_template('stats.html', table=stats['table'])
-    else:
-        return render_template('index.html')
+        user_selected_sort = request.form["sort_by"].title()
+        
+        if user_selected_sort == "Last Played":
+            user_selected_sort = "Last Played(Days)"
 
-if __name__ == '__main__':
+        if user_selected_sort == "Mastery Level" or user_selected_sort == "Last Played":
+            table.reversesort = True
+
+        table.sortby = user_selected_sort
+        table_html = table.get_html_string()
+        
+        return render_template("index.html", table=table_html)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
-
--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from data import champion_dict, api_key, get_champion_mastery_data, organize_champion_data
 from prettytable import PrettyTable
+import os
 
-summoner_name = str(input("Enter the summoner's IGN: "))
-data = get_champion_mastery_data(api_key, summoner_name)
-while data == -1: 
-    summoner_name = str(input("Summoner does not exist. Please try again: "))
+ongoing = True
+while ongoing:
+    summoner_name = str(input("Enter the summoner's IGN: "))
     data = get_champion_mastery_data(api_key, summoner_name)
-    
-data = organize_champion_data(data, champion_dict)
+    while data == -1: 
+        summoner_name = str(input("Summoner does not exist. Please try again: "))
+        data = get_champion_mastery_data(api_key, summoner_name)
+        
+    data = organize_champion_data(data, champion_dict)
 
-# print table
-table = PrettyTable()
-table.junction_char = "o"
-table.add_column("Champion", data["champion_names"])
-table.add_column("Mastery Level", data["champion_levels"])
-table.add_column("Mastery Points", data["champion_points"])
-table.add_column("Last Played(Days)", data["last_played"])
-table.add_column("Mastery Points Until Next Level", data["points_until_next_level"])
-table.add_column("Chest Granted", data["chests_granted"])
+    # print table
+    table = PrettyTable()
+    table.junction_char = "o"
+    table.add_column("Champion", data["champion_names"])
+    table.add_column("Mastery Level", data["champion_levels"])
+    table.add_column("Mastery Points", data["champion_points"])
+    table.add_column("Last Played(Days)", data["last_played"])
+    table.add_column("Mastery Points Until Next Level", data["points_until_next_level"])
+    table.add_column("Chest Granted", data["chests_granted"])
 
-print("What would you like to sort by?")
-user_sort = str(input('Sort By "Champion"\n"Mastery Level"\n"Mastery Points"\n"Last Played"\n"Mastery Points Until Next Level"\n"Chest Granted": ')).title()
-while user_sort not in ["Champion", "Mastery Level", "Mastery Points", "Last Played", "Mastery Points Until Next Level", "Chest Granted"]:
-    user_sort = str(input("Invalid input. Enter one of the above options: ")).title()
+    print("What would you like to sort by?")
+    user_sort = str(input('Sort By "Champion"\n"Mastery Level"\n"Mastery Points"\n"Last Played"\n"Mastery Points Until Next Level"\n"Chest Granted": ')).title()
+    while user_sort not in ["Champion", "Mastery Level", "Mastery Points", "Last Played", "Mastery Points Until Next Level", "Chest Granted"]:
+        user_sort = str(input("Invalid input. Enter one of the above options: ")).title()
 
-if user_sort == "Last Played":
-    user_sort = "Last Played(Days)"
+    if user_sort == "Last Played":
+        user_sort = "Last Played(Days)"
 
-if user_sort == "Mastery Level" or user_sort == "Mastery points until next level":
-    table.reversesort = True
+    if user_sort == "Mastery Level" or user_sort == "Last Played":
+        table.reversesort = True
 
-table.sortby = user_sort
-print(table)
+    table.sortby = user_sort
+    print(table)
 
-input("Press any key to exit. ")
+    end = input("End program? (y/n): ")
+    if end == "y":
+        ongoing = False
+    os.system("cls")
 
 
